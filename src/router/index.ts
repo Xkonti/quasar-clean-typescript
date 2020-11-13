42de2b9b2ff18fff3b56c122f70eaf5bd74bbb83
Xkonti <online.xkonti@gmail.com>
2020-11-13 17:39:47 -0500

    Install the vuex-module-decorators and cleanup store instance

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { route } from 'quasar/wrappers'
 import VueRouter from 'vue-router'
 import { Store } from 'vuex'
-import { StateInterface } from '../store'
 import routes from './routes'
 
 /*
@@ -9,7 +8,7 @@ import routes from './routes'
  * directly export the Router instantiation
  */
 
-export default route<Store<StateInterface>>(function ({ Vue }) {
+export default route<Store<unknown>>(function ({ Vue }) {
   Vue.use(VueRouter)
 
   const Router = new VueRouter({
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,14 @@
 import { store } from 'quasar/wrappers'
-import Vuex from 'vuex'
+import Vuex, { Store } from 'vuex'
 
-// import example from './module-example';
-// import { ExampleStateInterface } from './module-example/state';
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation
- */
-
-export interface StateInterface {
-  // Define your own store structure, using submodules if needed
-  // example: ExampleStateInterface;
-  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  example: unknown;
-}
+export let storeInstance: Store<unknown>
 
 export default store(function ({ Vue }) {
   Vue.use(Vuex)
-
-  const Store = new Vuex.Store<StateInterface>({
-    modules: {
-      // example
-    },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode only
+  const store = new Store<unknown>({
+    modules: {},
     strict: !!process.env.DEBUGGING
   })
-
-  return Store
+  storeInstance = store
+  return store
 })
