a32227bef6b27ce91784a20aa3e122e7f889e204
Xkonti <online.xkonti@gmail.com>
2020-11-14 01:32:05 -0500

    Clean-up the default router

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ import routes from './routes'
 export default route<Store<unknown>>(function ({ Vue }) {
   Vue.use(VueRouter)
 
-  const Router = new VueRouter({
+  return new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
 
@@ -21,6 +21,4 @@ export default route<Store<unknown>>(function ({ Vue }) {
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
   })
-
-  return Router
 })
