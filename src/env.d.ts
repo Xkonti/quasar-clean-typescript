fa2a411570ef7c7b35c6bbb11f5edca5a2e93684
Beniamin Dudek <xkonti@xkonti.tech>
2022-04-27 21:04:44 -0400

    Update rest of the content

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,9 @@
+/* eslint-disable */
+
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: string
-    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined
-    VUE_ROUTER_BASE: string | undefined
+    NODE_ENV: string;
+    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
+    VUE_ROUTER_BASE: string | undefined;
   }
 }
